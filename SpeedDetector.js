67d84b95c190e936ee899c249ef7538e850dd25c
Stephen Bowen <bowensteven17@gmail.com>
2024-03-25 22:11:34 +0300

    Speed Detector

--- SpeedDetector.js
+++ SpeedDetector.js
@@ -1,16 +1,16 @@
 let speed = parseFloat(prompt("Enter the speed :"));//gets input from the user and assigns it the variable speed as a float type
-speedDetector(speed);//invokes the function speedDetector
+speedDetector(speed);//invokes the function speedDetector and passes speed as the parameter
 function speedDetector (speed){//function declaration that takes the parameter 'speed'
     
     const speedLimit = 70;
     if(speed<=speedLimit){//condition for checking the speed range in reference to the speed limit
-        console.log('OK');//prints 'OK' if the speed is less than 70
+        console.log('OK');
     }else{
         let demeritPoints = (speed-speedLimit)/5;//calculates the demerit points for speeds over 70
         if (demeritPoints>12){//checks for speeds with demerit points over 12
-            console.log('License suspended');//prints 'License suspended'for demerit points over 12
+            console.log('License suspended');/
           }else{
-            console.log(`Points: ${demeritPoints}`);//prints the demerit points for points less than 12
+            console.log(`Points: ${demeritPoints}`);
     }
 }
 }
