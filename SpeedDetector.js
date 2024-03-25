e7fbbad66d87f583bec4fa78864426b87af08ff0
Stephen Bowen <bowensteven17@gmail.com>
2024-03-25 08:51:57 +0300

    Speed Detector

--- SpeedDetector.js
+++ SpeedDetector.js
@@ -3,7 +3,7 @@ speedDetector(speed);
 function speedDetector (speed){//function declaration that takes the parameter 'speed'
     const speedLimit = 70;
     if(speed<=speedLimit){//condition for checking the speed range in reference to the speed limit
-        console.log('OK');//console log 'OK' if the speed is less than 70
+        console.log('OK');//prints 'OK' if the speed is less than 70
     }else{
         let demeritPoints = (speed-speedLimit)/5;//calculates the demerit points for speeds over 70
         if (demeritPoints>12){//checks for speeds with demerit points over 12
