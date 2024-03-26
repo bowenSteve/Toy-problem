7fd76b6d275278189fadc503ec662abc680b2c23
Stephen Bowen <bowensteven17@gmail.com>
2024-03-26 21:42:00 +0300

    Student Grade Generator

--- StudentGradeGenerator.js
+++ StudentGradeGenerator.js
@@ -1,23 +1,24 @@
 function checkStudentMarks(){//function for capturing the marks is declared
     let marks = parseInt(prompt('Enter your marks : '))//inputs the entered marks and assigns the variable 'marks'
     if(marks>=0 && marks<=100){//checks for marks validity
-        gradeMarks(marks);//calls the function for grading if the marks are valid and passes the marks as the parameter
-    }else{
+        const grade =gradeMarks(marks);// a variable grade is declared and the output of gradeMarks function is assigned to it
+        console.log(`Your Grade is: ${grade}`)//prints the grade to the user
+      }else{
         console.log('Enter Valid Marks!');
     }
 }
-checkStudentMarks();//function for checking students' marks is called.
 
 function gradeMarks(studentMarks){//function for grading is declared
   if(studentMarks>79){
-    console.log('Student Grade: A');
+    return 'A';
   }else if(studentMarks>=60 && studentMarks<=79){
-    console.log('Student Grade: B');
+    return 'B'
   }else if(studentMarks>49 && studentMarks<=59){
-    console.log('Student Grade: C');
+    return 'C'
   }else if(studentMarks>=40 && studentMarks<=49){
-    console.log('Student Grade: D');
+    return 'D'
   }else{
-    console.log('Student Grade: E');
+    return 'E'
   }
 }
+checkStudentMarks();//function for checking students' marks is called.
